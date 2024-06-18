754c1bb87833841eb313b2ce53f7553ef5eed7be
pgterryc <154353399+pgterryc@users.noreply.github.com>
2024-06-18 12:16:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,7 @@ if ingredients_list :
     #st.write(ingredients_list)
     #st.text(ingredients_list)
     ingredients_string = ""
+    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
